9e2dbdc36f6cbec254112768d381432a5f8b622c
Devanathan-718549 <deva.narasimhan@gmail.com>
2018-05-25 10:38:23 +0530

    Python tutorial and basic command

---
+++ Snippetcode-python.py
@@ -0,0 +1,70 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+print("Hello World")
+for i in range(0,5):
+    print(i)
+    
+if True:
+   print ("Answer")
+   print ("True")
+else:
+   print ("Answer")
+   print ("False")
+   
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print (counter)
+print (miles)
+print (name)
+
+
+str = 'Hello World!'
+
+print (str)          # Prints complete string
+print (str[0])       # Prints first character of the string
+print (str[2:5])     # Prints characters starting from 3rd to 5th
+print (str[2:])      # Prints string starting from 3rd character
+print (str * 2)      # Prints string two times
+print (str + "TEST") # Prints concatenated string
+
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+tinylist = [123, 'john']
+
+print (list)          # Prints complete list
+print (list[0])       # Prints first element of the list
+print (list[1:3])     # Prints elements starting from 2nd till 4th 
+print (list[2:])      # Prints elements starting from 3rd element
+print (tinylist * 2)  # Prints list two times
+print (list + tinylist) # Prints concatenated lists
+
+
+tuple = ( 'abcd', 786 , 2.23, 'john', 70.2  )
+tinytuple = (123, 'john')
+
+print (tuple)           # Prints complete list
+print (tuple[0])        # Prints first element of the list
+print (tuple[1:3])      # Prints elements starting from 2nd till 3rd 
+print (tuple[2:])       # Prints elements starting from 3rd element
+print (tinytuple * 2)   # Prints list two times
+print (tuple + tinytuple) # Prints concatenated lists
+
+dict = {}
+dict['one'] = "This is one"
+dict[2]     = "This is two"
+
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+
+
+print (dict['one'])       # Prints value for 'one' key
+print (dict[2])           # Prints value for 2 key
+print (tinydict)          # Prints complete dictionary
+print (tinydict.keys())   # Prints all the keys
+print (tinydict.values()) # Prints all the values
